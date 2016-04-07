031f51f2126f26081581187669b0d05c8713ecd0
William Madruga <wmadruga@monexa.com>
2016-04-07 13:23:20 -0700

    Filtering test cases starting with 'off_' and skipping it

--- tester.js
+++ tester.js
@@ -10,6 +10,7 @@
   3. both should have the same name.
   4. server configuration should be created under config folder.
   5. server configuration should be named as version_servername.json.
+  6. if you want to skip a test case, start its name with 'off_'
 ********************************************************************* */
 
 const fs         = require('fs');
@@ -38,7 +39,7 @@ try {
     var q = async.queue(function (params, callback) {
       helper.make_request(conn_options, params.path, params.file);
       callback();
-    }, 5);
+    }, 1);
 
     tests_path = path.parse(file_or_directory + '/req');
     var current_test = tests_path.dir + '/' + tests_path.base;
@@ -48,15 +49,18 @@ try {
       }
       files.forEach( function( file, index ) {
         var full_file_path = current_test + '/' + file;
-        q.push({'path': full_file_path, 'file': file}, function (err) {
-          if (err) {
-            helper.error_message(err);
-          }
-        });
+        if (file.startsWith('off_')) {
+          console.log(colors.bgYellow.blue('skipping ' + file));
+        } else {
+          q.push({'path': full_file_path, 'file': file}, function (err) {
+            if (err) {
+              helper.error_message(err);
+            }
+          });
+        }
       });
     });
   }
-
 } catch (err) {
     helper.show_usage();
     helper.error_message(err);
